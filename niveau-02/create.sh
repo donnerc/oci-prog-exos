ef1a763f3718a9c62363cd7878e369d16a341aad
Cédric Donner <cedonner@gmail.com>
2013-10-24 00:21:40 +0200

    travail du jeudi 24 octobre 2013, 00:21:40 (UTC+0200)

--- niveau-02/create.sh
+++ niveau-02/create.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mkdir niveau-02
+cd niveau-02
 wget https://raw.github.com/donnerc/oci-prog-exos/master/niveau-02/chapitres.py
 curl https://raw.github.com/donnerc/oci-prog-exos/master/niveau-02/liste-taches.txt | python3 chapitres.py
-
+rm -f chapitres.py
+git add .
+git commit -m "Création des codes de base pour le niveau 02"
