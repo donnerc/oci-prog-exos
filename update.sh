ea253ff72f8b0ec1b7288648eab0437d03f7555e
Cédric Donner <cedonner@gmail.com>
2013-10-23 23:09:29 +0200

    travail du mercredi 23 octobre 2013, 23:09:29 (UTC+0200)

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ echo "téléchargement des mises à jour"
 git fetch upstream
 echo "assemblage des modifications dans votre dépôt"
 git checkout master
-git merge upstream/master 
+git merge upstream/master -m "mise à jour du dépôt à partir du dépôt original de donnerc"
