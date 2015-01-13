6a752a79369803a80168b03dfb366d64d7ce59ee
Cédric Donner <cedonner@gmail.com>
2015-01-13 13:58:28 +0000

    correction échange 3/8/5 et 6

--- niveau-03/chapitre-08-recursivite/04-sierpinski.py
+++ niveau-03/chapitre-08-recursivite/04-sierpinski.py
@@ -1,8 +1,23 @@
-### Tours de Hanoi
-### France IOI, partie 3, chapitre 8, problème 5
+taille = int(input())
 
-def hanoi()
-   pass
-
-
-print("Ce programme doit encore être développé")
+# on utilise une liste de liste de taille (taille x taille) pour représenter
+# le canevas
+canvas = [[' '] * taille for _ in range(taille)]
+ 
+def sierpinski(taille, x=0, y=0):
+ 
+   if taille == 1:
+          canvas[y][x] = '#'
+   else:
+      taille = taille // 2
+      sierpinski(taille, x, y)
+      sierpinski(taille, x + taille, y)
+      sierpinski(taille, x, y + taille)
+ 
+# dans France IOI, il n'est pas possible d'utiliser la fonction math.log2, ce qui 
+# nous oblige à la bricoler ...
+sierpinski(taille)
+ 
+# affichage du canvas sur la console
+for line in canvas:
+   print(''.join(line))
