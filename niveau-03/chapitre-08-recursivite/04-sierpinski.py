2f02c0bf2cfa807628553e50ac71b287b019ebc3
Cédric Donner <cedonner@gmail.com>
2015-01-13 13:45:04 +0000

    amélioration du code de 3/8/4 Sierpinski

--- niveau-03/chapitre-08-recursivite/04-sierpinski.py
+++ niveau-03/chapitre-08-recursivite/04-sierpinski.py
@@ -1,11 +1,23 @@
-n = int(input())
+taille = int(input())
 
-def developper(n):
-   if n == 0:
-      # base de case
-      return "0"
+# on utilise une liste de liste de taille (taille x taille) pour représenter
+# le canevas
+canvas = [[' '] * taille for _ in range(taille)]
+ 
+def sierpinski(taille, x=0, y=0):
+ 
+   if taille == 1:
+          canvas[y][x] = '#'
    else:
-      # cas récursif
-      return  "(" + developper(n-1) + " + " + developper(n-1) + ")"
-      
-print("0 = ", developper(n))
+      taille = taille // 2
+      sierpinski(taille, x, y)
+      sierpinski(taille, x + taille, y)
+      sierpinski(taille, x, y + taille)
+ 
+# dans France IOI, il n'est pas possible d'utiliser la fonction math.log2, ce qui 
+# nous oblige à la bricoler ...
+sierpinski(taille)
+ 
+# affichage du canvas sur la console
+for line in canvas:
+   print(''.join(line))
